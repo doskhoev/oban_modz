2f82e7b96f70a0f1e3d2364789cc5368466d362f
adam.doskhoev <doskhoyev@gmail.com>
2020-06-22 18:45:25 +0300

    fix: change logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-const App = () => (
+const App: React.FunctionComponent = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="spider" />
       <h1>Хам ди моги?</h1>
     </header>
   </div>
