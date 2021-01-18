c421291f5c1b6dd42dd44d59179364b30d23b6d5
adam.doskhoev <doskhoyev@gmail.com>
2021-01-18 17:02:09 +0300

    fix: fix about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import * as React from 'react'
 
 export const About = () => {
   return (
-    <div className="font-light text-center m-10 text-lg">
-      <h1>И тут тоже пока пусто</h1>
+    <div className="font-light text-left m-10 text-lg">
+      <p>Мы собираем мёд на кавказе уже 10 лет и делаем это очень хорошо.</p>
+      <p>За качество отвечаем головой.</p>
+      <p>
+        Наши пчёлы рады трудиться для того, чтобы угостить вас этим натуральным
+        продуктом.
+      </p>
     </div>
   )
 }
